11b4b04507c7d0f4e34b309914d23a1ef73a519c
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-09-18 22:20:41 -0600

    refact proyect

--- la-cuponera-react/metro.config.js
+++ la-cuponera-react/metro.config.js
@@ -1,7 +1,17 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
