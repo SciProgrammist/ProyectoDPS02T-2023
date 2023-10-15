3ab50214e390cf68fba20a86397a1634778acbfb
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-10-15 09:59:18 -0600

    ultimos cambios

---
+++ la-cuponera-react/src/utis/styles.cupones.js
@@ -0,0 +1,27 @@
+/* fonts */
+export const FontFamily = {
+  bodyHeavy: "Inter-SemiBold",
+  h3: "Inter-Bold",
+  body: "Inter-Regular",
+  poppinsSemiBold: "Poppins-SemiBold",
+  poppinsRegular: "Poppins-Regular",
+};
+/* font sizes */
+export const FontSize = {
+  bodyHeavy_size: 16,
+  h3_size: 24,
+  size_sm: 14,
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+  tertiary: "#eef1f4",
+  primary: "#545f71",
+  colorGray_100: "rgba(255, 255, 255, 0.9)",
+  colorGainsboro: "rgba(217, 217, 217, 0.02)",
+};
+/* border radiuses */
+export const Border = {
+  br_5xs: 8,
+  br_base: 16,
+};
