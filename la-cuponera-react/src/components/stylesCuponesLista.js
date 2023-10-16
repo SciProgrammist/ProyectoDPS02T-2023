55e211d83f14cd438d8296f5c3fc5c958c87adea
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-10-15 18:56:46 -0600

    commit

--- la-cuponera-react/src/components/stylesCuponesLista.js
+++ la-cuponera-react/src/components/stylesCuponesLista.js
@@ -217,5 +217,43 @@ export const styles = StyleSheet.create({
 		alignContent: 'center',
         width:500,
 		height: 550,
-	}
+	},
+    centeredView: {
+        width: '100%',
+        height: 700,
+        margin:'auto',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      modalView: {
+        margin: 2,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      buttonOpen: {
+        backgroundColor: '#F194FF',
+      },
+      buttonClose: {
+        backgroundColor: '#2196F3',
+      },
+      textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      modalText: {
+        marginBottom: 15,
+        margin: 5,
+        padding: 2,
+        textAlign: 'center',
+      },
 });
