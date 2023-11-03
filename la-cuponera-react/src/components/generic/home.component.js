6d3ff9cd4807246362f2faca7cf7c7e5e641a041
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-11-02 19:41:12 -0600

    extraer la información de los usuarios desde la base de datos

--- la-cuponera-react/src/components/generic/home.component.js
+++ la-cuponera-react/src/components/generic/home.component.js
@@ -1,12 +1,39 @@
-import { RefreshControlBase } from "react-native";
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-import { styles } from "../../styles/styles";
+import { auth } from '../../utis/firebase';
+import { database } from '../../utis/firebase';
+import { useEffect, useState } from 'react';
 
-export default function HomePage() {
+export default function HomePage(props) {
+
+    const [usuario, setUsuario] = useState(auth.currentUser);
+    const [currentUsers, setCurrentUsers] = useState();
+    const [id, setId] = useState(usuario.uid);
+
+
+    useEffect(() => {
+        findById();
+    }, [usuario])
+
+
+
+    function findById() {
+        database
+            .ref('/usuarios/' + id)
+            .once('value')
+            .then(snapshot => {
+                setCurrentUsers(JSON.parse(JSON.stringify(snapshot.val()).replace("null,", '')))
+                console.log('User data: ', snapshot.val());
+            });
+    }
+
+    console.log(id);
     return (
-        <View>
-            <Text>Home Page</Text>
+        <View style={{ margin: 'auto', margin: 5, padding: 5, width: '100%', height: '100%', alignItems: 'center' }}>
+            <Text>Home Page </Text>
+            <Text>Usuario Ingresado: {currentUsers.nombre} </Text>
+            <Text>Correo: {currentUsers.email} </Text>
+            <Text>Estado: {currentUsers.estado} </Text>
         </View>
     );
 }
