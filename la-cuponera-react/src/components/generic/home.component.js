ba2b66bea19b2ad221862eb9bff9f3ed8202af56
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2023-11-04 22:56:53 -0600

    home page complete

--- la-cuponera-react/src/components/generic/home.component.js
+++ la-cuponera-react/src/components/generic/home.component.js
@@ -1,31 +1,16 @@
+
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { auth } from '../../utis/firebase';
 import { database } from '../../utis/firebase';
 import { useEffect, useState } from 'react';
+import { styles } from "../../styles/styleHomePage";
 
 export default function HomePage(props) {
 
     const [usuario, setUsuario] = useState(auth.currentUser);
     const [currentUsers, setCurrentUsers] = useState();
     const [id, setId] = useState(usuario.uid);
-
-
-    useEffect(() => {
-        findById();
-    }, [usuario, currentUsers, id])
-
-
-
-    function findById() {
-        database
-            .ref('/usuarios/' + id)
-            .once('value')
-            .then(snapshot => {
-                setCurrentUsers(JSON.parse(JSON.stringify(snapshot.val()).replace("null,", '')))
-           //     console.log('User data: ', snapshot.val());
-            });
-    }
 
     function validarCampos(objeto) {
         for (let clave in objeto) {
@@ -36,16 +21,76 @@ export default function HomePage(props) {
         return true; // Si todos los campos son válidos, devuelve verdadero
     }
 
+    useEffect(() => {
+        findById();
+    }, [usuario, currentUsers, id])
 
-   // console.log(id);
-   //  console.log(currentUsers);
-    
+
+    // console.log(id);
+    //  console.log(currentUsers);
+
+    function findById() {
+        database
+            .ref('/usuarios/' + id)
+            .once('value')
+            .then(snapshot => {
+                setCurrentUsers(JSON.parse(JSON.stringify(snapshot.val()).replace("null,", '')))
+                //     console.log('User data: ', snapshot.val());
+            });
+    }
+
     return (
-        <View style={{ margin: 'auto', margin: 5, padding: 5, width: '100%', height: '100%', alignItems: 'center' }}>
-            <Text>Home Page </Text>
-            <Text>Usuario Ingresado: {validarCampos(currentUsers) && currentUsers != undefined ? currentUsers.nombre : ''} </Text>
-            <Text>Correo: {validarCampos(currentUsers) && currentUsers != undefined ? currentUsers.email : ''} </Text>
-            <Text>Estado: {validarCampos(currentUsers) && currentUsers != undefined ? currentUsers.estado : ''} </Text>
+        <View style={styles.profile}>
+            <View style={[styles.iphone143, styles.iphone143Position]}>
+
+                <Image
+                    style={styles.iphone143Child}
+                    resizeMode="cover"
+                    source={{ uri: `${validarCampos(currentUsers) && currentUsers != undefined ? currentUsers.img : ''}` }}
+                />
+                <View style={[styles.nameParent, styles.parentLayout]}>
+                    <Text style={[styles.name, styles.nameTypo]}>Name</Text>
+                    <View style={[styles.rectangleParent, styles.groupChildLayout]}>
+                        <View style={[styles.groupChild, styles.groupChildLayout]} />
+                        <Text style={[styles.melissaPeters, styles.textTypo]}>
+                            {validarCampos(currentUsers) && currentUsers != undefined ? currentUsers.nombre : ''}
+                        </Text>
+                    </View>
+                </View>
+                <View style={[styles.emailParent, styles.parentLayout]}>
+                    <Text style={[styles.name, styles.nameTypo]}>Email</Text>
+                    <View style={[styles.rectangleParent, styles.groupChildLayout]}>
+                        <View style={[styles.groupChild, styles.groupChildLayout]} />
+                        <Text style={[styles.melissaPeters, styles.textTypo]}>
+                            {validarCampos(currentUsers) && currentUsers != undefined ? currentUsers.email : ''}
+                        </Text>
+                    </View>
+                </View>
+                <View style={[styles.passwordParent, styles.parentLayout]}>
+                    <Text style={[styles.name, styles.nameTypo]}>Password</Text>
+                    <View style={[styles.rectangleParent, styles.groupChildLayout]}>
+                        <View style={[styles.groupChild, styles.groupChildLayout]} />
+                        <Text style={[styles.text, styles.textTypo]}>************</Text>
+                    </View>
+                </View>
+                <View style={[styles.dateOfBirthParent, styles.parentLayout]}>
+                    <Text style={[styles.name, styles.nameTypo]}>Date of Birth</Text>
+                    <View style={[styles.rectangleParent, styles.groupChildLayout]}>
+                        <View style={[styles.groupChild, styles.groupChildLayout]} />
+                        <Text style={[styles.melissaPeters, styles.textTypo]}>
+                            23/05/1995
+                        </Text>
+                    </View>
+                </View>
+                <View style={[styles.countryregionParent, styles.parentLayout]}>
+                    <Text style={[styles.name, styles.nameTypo]}>Country/Region</Text>
+                    <View style={[styles.rectangleParent, styles.groupChildLayout]}>
+                        <View style={[styles.groupChild, styles.groupChildLayout]} />
+                        <Text style={[styles.melissaPeters, styles.textTypo]}>Nigeria</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
+
